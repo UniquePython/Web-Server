a2dfead5911233500b0e26b2ec98fb7210568d29
Ananyo Bhattacharya <ananyobhattacharya.14.15.0012@gmail.com>
2024-05-25 21:14:41 +0530

    Improved functionality and styling

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/index.html')
+
+@app.route('/index.html')
+def home():
+    return render_template("index.html")
+
+@app.route('/works.html')
+def works():
+    return render_template("works.html")
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
