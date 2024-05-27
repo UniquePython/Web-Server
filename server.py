a0141729ffbc1ebc0c8d09ae25e1cf73d46948ca
Ananyo Bhattacharya <ananyobhattacharya.14.15.0012@gmail.com>
2024-05-27 14:51:00 +0530

    All done

--- server.py
+++ server.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
+import csv
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return redirect('/index.html')
 
-@app.route('/index.html')
-def home():
-    return render_template("index.html")
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/works.html')
-def works():
-    return render_template("works.html")
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('thankyou.html')
+    else:
+        return redirect('/')
 
-@app.route('/about.html')
-def about():
-    return render_template("about.html")
-
-@app.route('/contact.html')
-def contact():
-    return render_template("contact.html")
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n\nEmail: {email} \nSubject: {subject} \nMessage: {message}")
 
 if __name__ == '__main__':
     app.run(debug=True)
